082bafaf57433f890e00bc581e922f730aab1128
SteampunkWhales <100243497+SteampunkWhales@users.noreply.github.com>
2023-05-09 00:17:24 -0400

    copied file structure of regular blog

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-require()
+var bodyParser = require('body-parser')
+const ejs = require('ejs');
+const express = require('express');
+const _ = require("lodash");
+
+const app = express();
+
+app.get('/', (rec, res)=> {
+    res.send('hello world')
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000 :3")
+});
